bd899f46d02935c4ac6cf50614b3fea0292841ce
fardeen <fardeenkhan63574@gmail.com>
2026-01-22 12:41:51 +0530

    update

--- class-2/script.js
+++ class-2/script.js
@@ -1,15 +1,18 @@
-let express=require("express")
-let app=express()    //express initialization
+    let express=require("express")
+    let app=express()    //express initialization
 
-app.get("/",(req,res)=>{
-res.send("hello world")
+    app.get("/",(req,res)=>{
+    res.send("hello world")
 
-})
+    })
 
-app.get("/about",(req,res)=>{
-    res.send("this is my about page")
-})
+    app.get("/about",(req,res)=>{
+        res.send("this is my about page")
+    })
 
 
 
-app.listen(3000)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
